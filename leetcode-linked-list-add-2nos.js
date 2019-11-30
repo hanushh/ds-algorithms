6bd28326033bacf8c0e9fd3b0019f455c8b94e2f
Hanush H Nair <hnair@adobe.com>
2019-11-30 11:15:05 +0530

    Update leetcode-linked-list-add-2nos.js

--- leetcode-linked-list-add-2nos.js
+++ leetcode-linked-list-add-2nos.js
@@ -11,33 +11,33 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    
-    
-    var o = new ListNode(), oc= o,l1c=l1,l2c=l2, carry=0;
-    
-    
-    while(l1c || l2c  || (carry >0)){
-        
-        let l1v = (l1c && l1c.val)?l1c.val : 0;
-        let l2v = (l2c && l2c.val)?l2c.val : 0;
-        
-        let sum = carry +l1v + l2v;
+var addTwoNumbers = function (l1, l2) {
+
+
+    var o = new ListNode(), oc = o, l1current = l1, l2current = l2, carry = 0;
+
+
+    while (l1current || l2current || (carry > 0)) {
+
+        let l1value = (l1current && l1current.val) ? l1current.val : 0;
+        let l2value = (l2current && l2current.val) ? l2current.val : 0;
+
+        let sum = carry + l1valuealue + l2value;
         oc.val = sum % 10;
-        
-        carry = (sum >9)?(Math.floor(sum/10)):0;
-        
-        l1c = l1c && l1c.next;
-        l2c = l2c && l2c.next;
-        
-        if(l1c || l2c || (carry >0)){
-            oc.next =new ListNode();
-            
+
+        carry = (sum > 9) ? (Math.floor(sum / 10)) : 0;
+
+        l1current = l1current && l1current.next;
+        l2current = l2current && l2current.next;
+
+        if (l1current || l2current || (carry > 0)) {
+            oc.next = new ListNode();
+
             oc = oc.next;
         }
-        
+
     }
     return o;
-    
-    
+
+
 };
