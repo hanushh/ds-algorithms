4d921ca380cc44f5908f0dac6679fbe65412cb2d
Hanush H Nair <hnair@adobe.com>
2019-12-09 11:19:03 +0530

    change data type

--- hackerrank/max-occ.js
+++ hackerrank/max-occ.js
@@ -2,20 +2,21 @@
 function maximumOccurringCharacter(text) {
     // Write your code here
 
-    let txtArr = text.split(''), hashMap = {};
+    let txtArr = text.split(''), hashMap = new Map();
 
     for (let i = 0; i < txtArr.length; i++) {
-        if (hashMap[txtArr[i]]) {
-            hashMap[txtArr[i]]++;
+        if (hashMap.has(txtArr[i])) {
+            let incremented = hashMap.get(txtArr[i]) + 1;
+
+            hashMap.set(txtArr[i], incremented);
         } else {
-            hashMap[txtArr[i]] = 1;
+            hashMap.set(txtArr[i], 1);
         }
 
     }
 
-    let hashKeys = Object.keys((hashMap));
+    let hashKeys = Array.from(hashMap.keys());
     hashKeys.sort((a, b) => (hashMap[b] - hashMap[a]));
-    console.log(hashMap, hashKeys);
     return hashKeys[0];
 }
 console.log(maximumOccurringCharacter("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"));
