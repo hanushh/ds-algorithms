83d3d039a1dd78f639cdef73915f4a7fde498588
Hanush H Nair <hanushh@gmail.com>
2020-09-03 21:59:48 +0530

    merge-intervals

--- leetcode/merge-intervals.js
+++ leetcode/merge-intervals.js
@@ -6,49 +6,35 @@
  */
 var merge = function (intervals) {
 
-
-
     let mergedIntervals = [];
-
-
-
     const sortedIntervals = intervals.sort((a, b) => {
-
         if (a[0] < b[0]) {
             return -1;
         }
         if (a[0] > b[0]) {
             return 1;
         }
-
         // names must be equal
         return 0;
     });
 
 
     sortedIntervals.forEach(interval => {
-
         if (mergedIntervals.length === 0) {
             mergedIntervals.push(interval);
             return;
         }
 
         let lastInterval = mergedIntervals[mergedIntervals.length - 1]
-
-
         if (lastInterval[1] < interval[0]) {
             mergedIntervals.push(interval);
         } else if (lastInterval[1] < interval[1]) {
             lastInterval[1] = interval[1];
         }
-
-
     });
 
     console.log("Input:", intervals, "\nOutput:", mergedIntervals)
-
-
-
+    return mergedIntervals;
 };
 
 
