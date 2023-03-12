fd25918a7ee3ce8c396c9aa60dcfcc2df488f215
Hanush H Nair <hanushh@gmail.com>
2023-03-12 10:32:32 +0530

    next-app

--- components/Homepage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomePage = () => {
-
-
-    return <span >t</span>
-
-}
-export default HomePage;
--- pages/tictac.js
+++
@@ -1,5 +0,0 @@
-import TicTac from '../components/tictac/TicTac';
-import state from '../components/tictac/TicTac';
-export default function () {
-    return (<TicTac></TicTac>);
-}
